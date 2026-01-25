37e0625ee7d43a36f09794181774989153cb04bc
aaditya2357 <aaditya2357@gmail.com>
2026-01-25 14:52:50 +0530

    util

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+})
+
+
+
 
 /*
 import express from "express";
